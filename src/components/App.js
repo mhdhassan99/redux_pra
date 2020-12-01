d46d50481ed7e5bd6c877f7960ceb647905d53da
mhdhassan99 <mhassan.work99@gmail.com>
2020-12-01 13:09:39 -0500

    basic setup done

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddTodo from '..containers/AddTodo'
+
+const App = () => {
+    return (
+        <div>
+            <AddTodo />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const AddTodo = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AddTodo;
